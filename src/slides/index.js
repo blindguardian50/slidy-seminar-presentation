7fc3348af51743abdbde8b09725708da5e8482a3
David Egger <david.egga@gmx.at>
2023-11-20 20:51:13 +0100

    Update Presentation

--- src/slides/index.js
+++ src/slides/index.js
@@ -1,5 +1,6 @@
 import IntroSlides from 'bundle-text:../slides/intro/intro.html'
 import PatternSlides from 'bundle-text:../slides/patterns/patterns.html'
+import ExamplesSlides from 'bundle-text:../slides/examples/examples.html'
 import ToolsSlides from 'bundle-text:../slides/tools/tools.html'
 import OutroSlides from 'bundle-text:../slides/outro/outro.html'
 import GroupedBarWide from 'bundle-text:../images/grouped_bar_wide.svg'
@@ -13,6 +14,7 @@ function addHTML(selector, html) {
 function mergeDocument() {
     addHTML('body',  IntroSlides)
     addHTML('body',  PatternSlides)
+    addHTML('body',  ExamplesSlides)
     addHTML('body',  ToolsSlides)
     addHTML('body',  OutroSlides)
     addHTML('#grouped_bar_full_width',  GroupedBarWide)
@@ -25,12 +27,21 @@ function resizeElement(selector, from, to) {
 }
 
 function addClickResizing(selector, onClick) {
-    const e = document.querySelector('#grouped_bar_full_width')
+    const e = document.querySelector(selector)
     if (!e) return
     e.addEventListener('click', onClick)
+}
+
+function addRangeResizing(selector, onInput) {
+    const e = document.querySelector(selector)
+    if (!e) return
+    e.addEventListener('input', onInput)
 }
 
 document.addEventListener("DOMContentLoaded", (event) => {
     mergeDocument()
     addClickResizing('#grouped_bar_full_width', () => resizeElement('#grouped_bar_full_width_figure', '30%', '90%'))
+    addClickResizing('#iframe-example1-resizer', () => resizeElement('#iframe-example1-figure', '30%', '90%'))
+    addClickResizing('#iframe-example2-resizer', () => resizeElement('#iframe-example2-figure', '30%', '90%'))
+    addRangeResizing('#iframe-example2-range-resizer', (e) => resizeElement('#iframe-example2-figure', e.currentTarget.value + '%', e.currentTarget.value + '%'))
 });
